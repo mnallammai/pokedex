823a2aaa942710acbb6e26600c6bb95c7edd5c99
mnallammai <nalls.cool@gmail.com>
2021-05-31 23:48:41 +0530

    a new-branch commit

--- app/routes/monster.js
+++ app/routes/monster.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 
 export default class MonsterRoute extends Route {
     async model(){
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=200');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
         
         const result = await response.json();
         
